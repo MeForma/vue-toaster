665fb4500d4d7207c31f1df9d266fe59837d429f
Sacha STAFYNIAK <sacha@digisquad.io>
2022-08-29 11:19:36 +0200

    feat: add pausedAt timer prop

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -1,6 +1,7 @@
 // https://stackoverflow.com/a/3969760
 export default class Timer {
   constructor(callback, delay) {
+    this.pausedAt = 0
     this.startedAt = Date.now()
     this.callback = callback
     this.delay = delay
@@ -10,12 +11,14 @@ export default class Timer {
 
   pause() {
     this.stop()
-    this.delay -= Date.now() - this.startedAt
+    this.pausedAt = Date.now()
   }
 
   resume() {
     this.stop()
+    this.delay -= this.pausedAt - this.startedAt
     this.startedAt = Date.now()
+    this.pausedAt = 0
     this.timer = setTimeout(this.callback, this.delay)
   }
 
